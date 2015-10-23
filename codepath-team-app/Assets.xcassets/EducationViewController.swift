6530e7c4d009e3c9147651d15a688b24c851ac5a
Manuel Muñoz Solera <mamuso@mamuso.net>
2015-10-22 19:38:08 -0700

    Adding extra view controllers

---
+++ codepath-team-app/Assets.xcassets/EducationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EducationViewController.swift
+//  codepath-team-app
+//
+//  Created by mmuno on 10/22/15.
+//  Copyright © 2015 angel-magnolia-manuel. All rights reserved.
+//
+
+import UIKit
+
+class EducationViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
