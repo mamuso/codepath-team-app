165af31936b5e983da4a8526a3ba1a8c30939c81
Magnoolia <magnoliacm@gmail.com>
2015-11-01 11:40:31 -0800

    added swipe, button actions
    added swipe (needs polish), added archive button & placeholder button
    actions

--- codepath-team-app/ArticleViewController.swift
+++ codepath-team-app/ArticleViewController.swift
@@ -8,21 +8,92 @@
 
 import UIKit
 
+//mag to do:
+//  hook up bottom buttons
+//  fix swipe
+//  mark "left off" position
+
+
 class ArticleViewController: ViewController {
 //UIScrollViewDelegate
+    @IBOutlet var mainView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var placeholderArticle: UIImageView!
+    //@IBOutlet weak var placeholderArticle: UIImageView!
     @IBOutlet weak var articleTextLabel: UILabel!
     @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var archiveButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var doneTextLabel: UILabel!
     
     var articleText: String!
+    var titleText: String!
     var spaceUnderArticle: CGFloat!
+    var articleOriginalCenter: CGPoint!
+    var isArticleFavorited: CBool!
+    var isArticleArchived: CBool!
+    
+    
+    //@IBOutlet var swipeGestureRecognizer: UISwipeGestureRecognizer!
+    
+    @IBOutlet var swipeGestureRecognizerOverScroll: UISwipeGestureRecognizer!
+    
+    let swipeRecognizer = UISwipeGestureRecognizer()
+
+    @IBAction func onShare(sender: UIButton) {
+        print("share article")
+        //fake to fb
+    }
+    
+    @IBAction func onFavorite(sender: UIButton) {
+        print("favorite article")
+        if isArticleFavorited != nil {
+            print("onFavorite - already favorited")
+        }
+        else {
+            print("onFavorite - set")
+            isArticleFavorited = true
+            //mark as favorite & change icon
+            //treat same as in article list
+        }
+    }
+    
+    @IBAction func onArchive(sender: UIButton) {
+        if isArticleArchived != nil {
+            print("onArchive - already archived")
+        }
+        else {
+            print("onArchive - set")
+            isArticleArchived = true
+            //add to archived/read list
+            
+        }
+    }
+    
+    @IBAction func onNext(sender: UIButton) {
+        print("next article")
+        //jump to next article in article view list
+        
+    }
+    
+    //@IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
+    /* @IBAction func onPan(sender: UIPanGestureRecognizer) {
+        print("panGestureAction")
+        let point = sender.locationInView(view)
+        var velocity = sender.velocityInView(view)
+        var translation = sender.translationInView(view)
+        if sender.state == UIGestureRecognizerState.Began
+        {} else if sender.state == UIGestureRecognizerState.Changed
+        {}
+        else if sender.state == UIGestureRecognizerState.Ended
+        {}
+    }
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleText = "Title of Article"
         
         articleText = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc."
         
@@ -41,22 +112,31 @@ class ArticleViewController: ViewController {
         //set button options under "all done" text
         shareButton.frame.origin.y = articleTextLabel.frame.size.height + spaceUnderArticle * 2
         favoriteButton.frame.origin.y = articleTextLabel.frame.size.height + spaceUnderArticle * 2
+        archiveButton.frame.origin.y = articleTextLabel.frame.size.height + spaceUnderArticle * 2
         nextButton.frame.origin.y = articleTextLabel.frame.size.height + spaceUnderArticle * 2 //"next" or "more"
         
         
-        print("share origin y: \(shareButton.frame.origin.y)")
-        print("article height: \(articleTextLabel.frame.size.height)")
         
+        scrollView.addGestureRecognizer(swipeRecognizer)
+        //^^COMEBACK TO THIS & FIX - **scroll view area is not recognizing the swipe**
         
-        //print("height of article: \(articleTextLabel.frame.height)")
+        //mainView.addGestureRecognizer(swipeRecognizer) //only works on nav bar
+
+        //print("share origin y: \(shareButton.frame.origin.y)")
+        //print("article height: \(articleTextLabel.frame.size.height)")
+        
         //scrollView.contentSize = placeholderArticle.image!.size
-        //print("length of article: \(size)") //(count(articleText))
+        //print("length of article: \(count(articleText))")
         
         
     }
 
     //NEXT:
-    //  as scroll down, save greatest scroll position
+    //  as scroll down, SAVE greatest scroll position
+    //  id for each article?
+    //  hide tab controller on this page
+    
+    
     
     
     
